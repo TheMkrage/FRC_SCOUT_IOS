6cb5f5bdb04ff7f31210eb2256a2ae09ea0d17d4
TheMkrage <metoofarmer@yahoo.com>
2015-01-27 17:58:07 -0800

    setup teller view controller

--- FRC-Scout/level1PitTeleopViewController.m
+++ FRC-Scout/level1PitTeleopViewController.m
@@ -7,22 +7,83 @@
 //
 
 #import "level1PitTeleopViewController.h"
+#import "kragerPickerView.h"
+#import "checkBoxLabel.h"
 
-@interface level1PitTeleopViewController ()
+@interface level1PitTeleopViewController () {
+    IBOutlet UIScrollView *scrollView;
+    bool textFieldShouldEdit;
+    UITextField *activeTextField;
+    id activeAspect;
+}
+
 
 @end
 
 @implementation level1PitTeleopViewController
 
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.tabBarController.title = @"Teleop";
+    [self setFonts];
+    [self setDelegates];
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap:)];
+    singleTapGestureRecognizer.numberOfTapsRequired = 1;
+    singleTapGestureRecognizer.enabled = YES;
+    singleTapGestureRecognizer.cancelsTouchesInView = NO;
+    [scrollView addGestureRecognizer:singleTapGestureRecognizer];
+    
+    scrollView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+    
+    [self setData];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)viewDidLayoutSubviews {
+    NSLog(@"LAYED OUT");
+    scrollView.frame = CGRectMake(scrollView.frame.origin.x, scrollView.frame.origin.y, 320, self.view.frame.size.height);
+    scrollView.contentSize = CGSizeMake(320, 1600);
+    [scrollView  setCenter:CGPointMake(scrollView.center.x, scrollView.center.y - 62)];
+    [self.view layoutSubviews];
+    
+}
+
+-(void) setFonts {
+    
+}
+
+-(void) setData {
+    
+}
+
+-(void) setDelegates {
+    
+}
+
+-(void) singleTap:(UITapGestureRecognizer*)gesture {
+    NSLog(@"TAP");
+    [self turnOffActiveAspect];
+}
+
+- (void) turnOffActiveAspect {
+    if([activeAspect isKindOfClass:[kragerPickerView class]]) {
+        [activeAspect setHidden:YES];
+    } else {
+        [activeAspect resignFirstResponder];
+    }
+    
+}
+
+
 
 /*
 #pragma mark - Navigation

3b113bcfbbeb3cf82041fff6669fb0af8e9c50b0
Matthew Krager <MatthewKrager@Matthews-MacBook-Air.local>
2015-01-27 16:49:09 -0800

    pitTeleopViewController

---
+++ FRC-Scout/level1PitTeleopViewController.h
@@ -0,0 +1,13 @@
+//
+//  level1PitTeleopViewController.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 1/27/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface level1PitTeleopViewController : UIViewController
+
+@end
---
+++ FRC-Scout/level1PitTeleopViewController.m
@@ -0,0 +1,37 @@
+//
+//  level1PitTeleopViewController.m
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 1/27/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import "level1PitTeleopViewController.h"
+
+@interface level1PitTeleopViewController ()
+
+@end
+
+@implementation level1PitTeleopViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

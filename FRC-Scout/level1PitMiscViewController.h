16b189f490e43652943f120cca329edc3285dd72
TheMkrage <metoofarmer@yahoo.com>
2015-01-29 21:56:58 -0800

    created pitscoutmisc controller

---
+++ FRC-Scout/level1PitMiscViewController.h
@@ -0,0 +1,13 @@
+//
+//  level1PitMiscViewController.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 1/29/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface level1PitMiscViewController : UIViewController
+
+@end
---
+++ FRC-Scout/level1PitMiscViewController.m
@@ -0,0 +1,37 @@
+//
+//  level1PitMiscViewController.m
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 1/29/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import "level1PitMiscViewController.h"
+
+@interface level1PitMiscViewController ()
+
+@end
+
+@implementation level1PitMiscViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

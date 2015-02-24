870fe62c7802816ce572a923bed81c81c7db30ac
TheMkrage <metoofarmer@yahoo.com>
2015-02-23 21:44:23 -0800

    JSONObject made

---
+++ FRC-Scout/JSONObject.h
@@ -0,0 +1,13 @@
+//
+//  JSONObject.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 2/23/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONObject : NSObject
+
+@end
---
+++ FRC-Scout/JSONObject.m
@@ -0,0 +1,13 @@
+//
+//  JSONObject.m
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 2/23/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import "JSONObject.h"
+
+@implementation JSONObject
+
+@end

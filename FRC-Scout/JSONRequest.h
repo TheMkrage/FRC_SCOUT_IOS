0fd108dd07eec85e24a4b2a6826a6ef03a661018
TheMkrage <metoofarmer@yahoo.com>
2015-02-26 15:23:35 -0800

    JSON Request

---
+++ FRC-Scout/JSONRequest.h
@@ -0,0 +1,13 @@
+//
+//  JSONRequest.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 2/26/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONRequest : NSObject
+
+@end
---
+++ FRC-Scout/JSONRequest.m
@@ -0,0 +1,13 @@
+//
+//  JSONRequest.m
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 2/26/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import "JSONRequest.h"
+
+@implementation JSONRequest
+
+@end

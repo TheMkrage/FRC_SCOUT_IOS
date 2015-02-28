8b38ba6ca08c6a058d472c00a088bc3dc040b85a
TheMkrage <metoofarmer@yahoo.com>
2015-02-28 14:15:18 -0800

    removed uitoteview

--- FRC-Scout/ToteStack.m
+++ FRC-Scout/ToteStack.m
@@ -23,38 +23,6 @@
     return self;
 }
 
--(id) init {
-    self = [super init];
-    UIImageView* tote0 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Tote.png"]];
-    [tote0 setFrame:CGRectMake([self getCorrectX], [self getCorrectYForTote: 0] * 2, 30, 20)];
-    self.toteArray = [[NSMutableArray alloc]init];
-    
-    return self;
-}
-
--(int) getCorrectX {
-    if(self.stack == 1) {
-        return 32;
-    }else if(self.stack == 2) {
-        return 432;
-    }else if(self.stack == 3) {
-        return 600;
-    }else if(self.stack == 4) {
-        return 800;
-    }else if(self.stack == 5) {
-        return 990;
-    }else {
-        return 0;
-    }
-}
-
--(int) getCorrectYForTote: (int) toteNum {
-    if(toteNum == 0) {
-        
-    }
-    return 0;
-}
-
 
 
 

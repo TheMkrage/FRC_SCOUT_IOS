861acb14002ecd0d99a96217ca7b549c9b5f6180
TheMkrage <metoofarmer@yahoo.com>
2015-03-02 21:48:32 -0800

    Data Manger

---
+++ FRC-Scout/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 3/2/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ FRC-Scout/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 3/2/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end

500e85162313960a2273597c1a56c92b6217e615
TheMkrage <metoofarmer@yahoo.com>
2015-03-06 21:50:10 -0800

    pit scout view controller

---
+++ FRC-Scout/Level1PitScoutDisplayViewController.h
@@ -0,0 +1,13 @@
+//
+//  Level1PitScoutDisplayViewController.h
+//  FRC-Scout
+//
+//  Created by Matthew Krager on 3/6/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Level1PitScoutDisplayViewController : UIViewController
+
+@end

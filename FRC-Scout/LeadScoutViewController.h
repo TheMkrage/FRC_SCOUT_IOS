c6e7b773a6f9fa9e9afe064fefcc8862fd95983f
TheMkrage <metoofarmer@yahoo.com>
2015-03-08 22:44:09 -0700

    level2

--- FRC-Scout/LeadScoutViewController.h
+++ FRC-Scout/LeadScoutViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LeadScoutViewController : UIViewController
+@interface LeadScoutViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- FRC-Scout/LeadScoutViewController.m
+++ FRC-Scout/LeadScoutViewController.m
@@ -9,16 +9,40 @@
 #import "LeadScoutViewController.h"
 
 @interface LeadScoutViewController ()
-
+@property (strong, nonatomic) IBOutlet UITextField *markMatchAsPlayedTextField;
+@property (strong, nonatomic) IBOutlet UITextField *markMatchAsNotPlayedTextField;
+@property (strong, nonatomic) IBOutlet UIButton *markMatchAsPlayedButton;
+@property (strong, nonatomic) IBOutlet UIButton *markMatchAsNotPlayedButton;
+@property (strong, nonatomic) UITextField* activeAspect;
 @end
 
 @implementation LeadScoutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.markMatchAsNotPlayedTextField.keyboardType = UIKeyboardTypeNumberPad;
+    self.markMatchAsPlayedTextField.keyboardType = UIKeyboardTypeNumberPad;
+    
+    UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap:)];
+    singleTapGestureRecognizer.numberOfTapsRequired = 1;
+    singleTapGestureRecognizer.enabled = YES;
+    singleTapGestureRecognizer.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:singleTapGestureRecognizer];
+    
+    /*self.markMatchAsNotPlayedTextField.delegate = self;
+    self.markMatchAsPlayedTextField.delegate =self;*/
+}
+- (IBAction)markMatchAsPlayed:(id)sender {
+    //WAS HERE
 }
 
+-(void) singleTap: (UITapGestureRecognizer*)gesture {
+    NSLog(@"TAP");
+    [self turnOffActiveAspect];
+}
+- (void) turnOffActiveAspect {
+        [self.activeAspect resignFirstResponder];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -34,4 +58,8 @@
 }
 */
 
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    self.activeAspect = textField;
+    return YES;
+}
 @end
